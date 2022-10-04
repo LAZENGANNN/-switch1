58352d76c428457b813865c527b28827bb0f7472
LAZENGANNN <113790875+LAZENGANNN@users.noreply.github.com>
2022-10-04 18:36:54 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,100 @@
+﻿#include<iostream>
+#include<cmath>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+
+	//case1
+	/*int day;
+	cin >> day;
+	switch (day) {
+	case 1: cout << "понедельник" << endl;
+		break;
+	case 2: cout << "вторник" << endl;
+		break;
+	case 3: cout << "среда" << endl;
+		break;
+	case 4: cout << "четверг" << endl;
+		break;
+	case 5: cout << "пятница" << endl;
+		break;
+	case 6: cout << "суббота" << endl;
+		break;
+	case 7: cout << "воскресенье" << endl;
+		break;
+	default: cout << "ошибка номера";
+	}
+	return 0;*/
+
+	//case2
+	/*int mark;
+	cin >> mark;
+	switch (mark) {
+	case 1: cout << "плохо"; break;
+	case 2: cout << "неуд"; break;
+	case 3: cout << "удовл"; break;
+	case 4: cout << "хорошо"; break;
+	case 5: cout << "отлично"; break;
+	default: cout << "ошибка"; break;
+	}
+	return 0;*/
+
+	//case3
+	/*int month;
+	cin >> month;
+	switch (month) {
+	case 1:
+	case 2:
+	case 12: cout << "зима"; break;
+	case 3:
+	case 4:
+	case 5: cout << "весна"; break;
+	case 6:
+	case 7:
+	case 8: cout << "лето"; break;
+	case 9:
+	case 10:
+	case 11: cout << "осень"; break;
+
+
+	}*/
+
+	//case4
+	/*int month;
+	cin >> month;
+	switch (month) {
+	case 1:
+	case 3:
+	case 5:
+	case 7:
+	case 8:
+    case 10:
+	case 12: cout << "31"; break;
+	case 2: cout << "29"; break;
+	default: cout << "30"; break;
+
+
+
+	}
+	return 0;*/
+
+	//case5
+	/*int a;
+	int b;
+	int n;
+	int res;
+	cin >> a >> b >> n;
+	switch (n) {
+	case 1: res = a + b; break;
+	case 2: res = a - b; break;
+	case 3: res = a * b; break;
+	case 4: res = a / b; break;
+
+
+	}
+	cout << res;
+	return 0;*/
+
+	
+
+}
